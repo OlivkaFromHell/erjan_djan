b88f1227fe61c018c7bdad25261471d8d04af656
OlivkaFromHell <dagerrashid@mail.ru>
2021-08-05 16:42:49 +0300

    start msg stat function

--- msg_stat.py
+++ msg_stat.py
@@ -8,25 +8,31 @@ class Conversation:
 
     def __init__(self, chat_id, chat_name):
         self.chat_id = chat_id
-        self.chat_name = chat_namename
-
-    def register(self, msg):
-        pass
+        self.chat_name = chat_name
+        self.amount_msg = 0
 
 
 class ConversationMember(Conversation):
     def __init__(self, chat_id, chat_name, member_id, member_name):
-        super().__init__(self, chat_id, chat_name)
+        super().__init__(chat_id, chat_name)
         self.member_id = member_id
         self.member_name = member_name
+        self.member_amount_msg = {
+            'total': 0,
+            'audio_msg': 0,
+            'audio': 0,
+            'video': 0,
+            'explicit': 0
+        }
 
     def count_msg(self):
-        pass
+        # some message handling
+        self.amount_msg += 1
+        self.member_amount_msg += 1
 
-# ConversationMember(1, 'helsso', 123, 'rashid')
-# print(ConversationMember.chat_id)
 
-try:
-    1/0
-except Exception as e:
-    print(e)
+a = ConversationMember(1, 'helsso', 123, 'rashid')
+print(a.__dict__)
+
+if __name__ == '__main__':
+    print('hello')

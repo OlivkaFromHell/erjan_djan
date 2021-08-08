1232e369ecb42c51fb6d1cebb80cacc64e77644f
OlivkaFromHell <dagerrashid@mail.ru>
2021-08-08 22:18:21 +0300

    update msg_stat.py

--- msg_stat.py
+++ msg_stat.py
@@ -1,39 +1,119 @@
-import re
+from sqlalchemy import create_engine, func
+from sqlalchemy import Column, Integer, Boolean, String, Date, Time
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+
+from config import postgres_user, postgres_password, postgres_ip, postgres_db
+
+engine = create_engine(f'postgresql://{postgres_user}:{postgres_password}'
+                       f'@{postgres_ip}:5432/{postgres_db}')
+Base = declarative_base()
 
 
-class Conversation:
-    patterns = {
+class Conversation(Base):
+    __tablename__ = 'conversations'
 
-    }
+    id = Column(Integer, primary_key=True)
+    chat_id = Column(Integer)  # id чата
+    member_id = Column(Integer)  # vk id пользователя
+    member_name = Column(String)  # имя пользователя
+    current_date = Column(Date, default=datetime.date.today())
+    current_time = Column(Time(timezone=True), default=datetime.datetime.now().time().strftime('%H:%M:%S'))
+    photo = Column(Integer)  # кол-во фотографий
+    audio = Column(Integer)  # кол-во аудиозаписей
+    video = Column(Integer)  # кол-во видео
+    doc = Column(Integer)  # кол-во докумнтов
+    audio_msg = Column(Boolean)  # кол-во аудиосообщений
+    sticker = Column(Boolean)  # кол-во стикеров
 
-    def __init__(self, chat_id, chat_name):
-        self.chat_id = chat_id
-        self.chat_name = chat_name
-        self.amount_msg = 0
-
-
-class ConversationMember(Conversation):
-    def __init__(self, chat_id, chat_name, member_id, member_name):
-        super().__init__(chat_id, chat_name)
-        self.member_id = member_id
-        self.member_name = member_name
-        self.member_amount_msg = {
-            'total': 0,
-            'audio_msg': 0,
-            'audio': 0,
-            'video': 0,
-            'explicit': 0
-        }
-
-    def count_msg(self):
-        # some message handling
-        self.amount_msg += 1
-        self.member_amount_msg += 1
+    def __repr__(self):
+        return f"Conversation {self.chat_name}: {self.chat_id}"
 
 
-a = ConversationMember(1, 'helsso', 123, 'rashid')
-print(a.__dict__)
-a = 'priver'
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
+session = Session()
 
-if __name__ == '__main__':
-    print('hello')
+
+# Create
+def insert_msg(chat_id, member_id, member_name,
+               photo_stat, audio_stat, audio_msg_stat, video_stat, doc_stat, sticker_stat):
+    new_data = Conversation(
+        chat_id=chat_id,
+        member_id=member_id,
+        member_name=member_name,
+        photo=photo_stat,
+        audio=audio_stat,
+        audio_msg=audio_msg_stat,
+        video=video_stat,
+        doc=doc_stat,
+        sticker=sticker_stat
+    )
+
+    with Session() as session:
+        with session.begin():
+            session.add(new_data)
+            session.commit()
+
+
+def check_registration(chat_id, user_id):
+    session.begin()
+    is_user_exist = session.query(Conversation).filter_by(chat_id=chat_id, member_id=user_id).all()
+    session.close()
+    if is_user_exist:
+        return True
+    return False
+
+
+def count_user_msg(chat_id, member_id):
+    with Session() as session:
+        with session.begin():
+            res = session.query(Conversation).filter_by(chat_id=chat_id, member_id=member_id).count()
+    return res
+
+
+def count_conversation_msg(chat_id):
+    with Session() as session:
+        with session.begin():
+            res = session.query(Conversation).filter_by(chat_id=chat_id).count()
+    return res
+
+
+def find_top_5_users(chat_id):
+    with Session() as session:
+        with session.begin():
+            top = session.query(Conversation.member_name, Conversation.member_id,
+                                func.count().label('top')) \
+                .filter_by(chat_id=chat_id).group_by(Conversation.member_name, Conversation.member_id) \
+                .order_by(func.count().label('top').desc()).limit(5)
+    return top
+
+
+def get_chat_statistic(chat_id):
+    with Session() as session:
+        with session.begin():
+            count_msg = session.query(Conversation).filter_by(chat_id=chat_id).count()
+            audio_msg = session.query(Conversation).filter_by(chat_id=chat_id, audio_msg=True).count()
+            photo = session.query(Conversation).filter_by(chat_id=chat_id, photo=True).count()
+            audio = session.query(Conversation).filter_by(chat_id=chat_id, audio=True).count()
+            video = session.query(Conversation).filter_by(chat_id=chat_id, video=True).count()
+            doc = session.query(Conversation).filter_by(chat_id=chat_id, video=True).count()
+            sticker = session.query(Conversation).filter_by(chat_id=chat_id, sticker=True).count()
+
+    chat_stat = """Статистика за весь период
+📧 Сообщений: {count_msg}
+🎵 Голосовых: {audio_msg}
+📷 Фото: {photo}
+🎧 Аудио: {audio}
+📹 Видео: {video}
+📑 Документов: {doc}
+💩 Стикеров: {sticker}
+""".format(count_msg=count_msg, audio_msg=audio_msg,
+           photo=photo, audio=audio, video=video, doc=doc, sticker=sticker)
+    return chat_stat
+
+
+if __name__ == "__main__":
+    res = check_registration(1231, 129)
+    print(res)

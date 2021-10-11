f377a6a6492c92f529bc4a5393feaba1f2859678
OlivkaFromHell <dagerrashid@mail.ru>
2021-10-11 10:23:59 +0300

    update morning_weather.py to use it in cron

--- morning_weather.py
+++ morning_weather.py
@@ -1,11 +1,10 @@
-import schedule
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from time import sleep
 
 from config import token
-from info import number_base
 from weather import for_day_weather
+
 
 def send_msg(id, text):
     sleep(0.5)
@@ -13,6 +12,7 @@ def send_msg(id, text):
             {'chat_id': id,
              'message': text,
              "random_id": 0})
+
 
 def send_weather():
     send_msg(2, for_day_weather())
@@ -23,7 +23,5 @@ vk = vk_session.get_api()
 groupId = 202528897
 longpoll = VkBotLongPoll(vk_session, groupId)
 
-schedule.every().day.at("08:00").do(send_weather)
-
-while True:
-    schedule.run_pending()
+if __name__ == "__main__":
+    send_weather()
